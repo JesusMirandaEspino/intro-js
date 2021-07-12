245a2141c7ea684fc160d82eb59c75a327927008
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-12 16:40:00 -0500

    desestructurar

--- src/index.js
+++ src/index.js
@@ -26,3 +26,31 @@ const { nombreNormal, edadReal, origen:{ pais, estado  } } = retornarPersona( pe
 
 console.log(  pais, estado );
 console.log( nombreNormal );
+
+
+const personajes = [ 'Mutuelo', 'Mutuell', 'Ramo' ];
+
+const [ mutuelo, , ramo  ] = personajes;
+
+console.log( mutuelo, ramo );
+
+const retornaArreglo = () => {
+    return [ 'ABC', 123 ];
+}
+
+
+const [ letras, numeros ] = retornaArreglo();
+
+console.log( letras, numeros );
+
+
+const otro = ( valor ) => {
+    return [ valor, ()=>{console.log( 'Hola mundo' );} ];
+}
+
+
+const [ nombre1, setNombre ] = otro('Jesus');
+
+
+console.log( nombre1 );
+setNombre();
