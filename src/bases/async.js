118628e0c812d8a3997f73893efc209350a9d04c
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-14 22:48:03 -0500

    operadores ternarios

---
+++ src/bases/async.js
@@ -0,0 +1,18 @@
+
+const getImage = async () => {
+
+
+    try{
+        const api_key = 'Qmar0HBJRlLnLaX5QQMHEsae5AiucqSG';
+        const resp    = await fetch( `http://api.giphy.com/v1/gifs/trending?api_key=${api_key}` );
+        const data = await resp.json(  );
+
+        console.log( data.data[15].bitly_gif_url );
+
+    }catch(err){
+        console.log( err );
+    }
+}
+
+
+getImage();
--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
+const activo = true;
 
 
+// const mensaje = ( activo ) ? 'Activo' : 'inactivo';
+
+const mensaje = !activo && 'Activo';
 
 
+console.log( mensaje );
 
-
-const getImage = async () => {
-
-
-    try{
-        const api_key = 'Qmar0HBJRlLnLaX5QQMHEsae5AiucqSG';
-        const resp    = await fetch( `http://api.giphy.com/v1/gifs/trending?api_key=${api_key}` );
-        const data = await resp.json(  );
-
-        console.log( data.data[15].bitly_gif_url );
-
-    }catch(err){
-        console.log( err );
-    }
-}
-
-
-getImage();
