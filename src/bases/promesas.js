dc61a6338e6969734adc5fd0d268909bbc28580f
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-14 22:25:49 -0500

    fetch API

---
+++ src/bases/promesas.js
@@ -0,0 +1,38 @@
+import { getHeroeByid, getHeroeByOwner } from './bases/exports';
+
+
+const promesa = new Promise( ( resolve, reject )=>{
+    setTimeout( ()=>{
+        const heroes = getHeroeByid(2);
+        resolve( heroes );
+    }, 2000 );
+} );
+
+
+promesa.then( (heroes) => {
+    console.log( heroes );
+} )
+.catch( err => console.log( err ) );
+
+
+
+const getHeroeByidAsync = ( id ) => {
+
+   return  new Promise( ( resolve, reject )=>{
+        setTimeout( ()=>{
+            const heroes = getHeroeByid( id );
+            if(heroes){
+                resolve( heroes );             
+            }else{
+                reject( 'No se encontro el heroe con el id seleccionado' );
+            }
+
+        }, 2000 );
+    } );
+
+
+}
+
+
+getHeroeByidAsync(1).then( console.log  ).catch( console.warn );
+
--- src/index.js
+++ src/index.js
@@ -1,40 +1,18 @@
-import { getHeroeByid, getHeroeByOwner } from './bases/exports';
+const api_key = 'Qmar0HBJRlLnLaX5QQMHEsae5AiucqSG';
+
+const peticion = fetch( `http://api.giphy.com/v1/gifs/trending?api_key=${api_key}` );
 
 
-const promesa = new Promise( ( resolve, reject )=>{
-    setTimeout( ()=>{
-        const heroes = getHeroeByid(2);
-        resolve( heroes );
-    }, 2000 );
-} );
-
-
-promesa.then( (heroes) => {
-    console.log( heroes );
-} )
-.catch( err => console.log( err ) );
-
-
-
-const getHeroeByidAsync = ( id ) => {
-
-   return  new Promise( ( resolve, reject )=>{
-        setTimeout( ()=>{
-            const heroes = getHeroeByid( id );
-            if(heroes){
-                resolve( heroes );             
-            }else{
-                reject( 'No se encontro el heroe con el id seleccionado' );
-            }
-
-        }, 2000 );
-    } );
-
-
-}
-
-
-getHeroeByidAsync(10).then( (heroe) => {
-    console.log( heroe );
-} ).catch( err => console.log( err ) );
-
+peticion.then( resp =>  resp.json()   )
+    .then(  ( { data } )=> { 
+        const dataArray =  data 
+    
+        dataArray.forEach( item  => {
+            let img = document.createElement( 'img' );
+            let imgGif = item.bitly_gif_url; 
+            img.src = imgGif;
+            document.appendChild( img );
+        }  )
+    
+    } )
+    .catch( console.warn );
